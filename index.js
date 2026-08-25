f4a1f0acf965bd790b4df65a71404660850bc9cf
Samindigital <Samindigitalsolution@gmail.com>
2026-08-26 00:41:41 +0300

    Update Telegram bot token in index.js
    Replaced placeholder token with actual bot token.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
-const TOKEN = 'YOUR_BOT_TOKEN_HERE'; // <-- የBotFather ቶክንህን እዚህ አስገባ
+const TOKEN = 8803882724:AAFxQyifk9_snGYfdjiirs69X_XbJfoxtHY
 const bot = new TelegramBot(TOKEN, { polling: true });
 const app = express();
 
